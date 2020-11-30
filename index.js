9475023b3dd2c371b579794de29e23b8a3048323
WhiteArro <damitrafi6@gmail.com>
2020-11-30 11:49:30 +0100

    ranking and counting mechanism added

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Score = require('./models/ranking');
 
-//##########################        MIDDLEWARE        ######################
+mongoose.connect('mongodb://localhost:27017/colorGame', { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => {
+      console.log('MONGO CONNECTION OPEN')
+   })
+   .catch(err => {
+      console.log('OH NO MONGO ERROR !', err)
+   });
+
+//##########################        MIDDLEWARE
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
+app.use(methodOverride('_method'));
 
-//##########################        ROUTES         ######################
-
-app.get('/', (req, res) => {
-   res.render('index')
+//##########################        ROUTES
+// GAME
+app.get('/', async(req, res) => {
+   const record = await findRecord();
+   console.log(record)
+   res.render('index',{record})
 })
-
-app.post('/', (req, res) => {
+// SHOW RANKINGS
+app.get('/ranking', async(req, res) => {
+   const scores = await Score.find({});
+   const l =getLength(scores);
+   res.render('show',{scores,l})
+})
+// SUBMIT NEW RECORD
+app.post('/', async (req, res) => {
    console.log(req.body)
-   res.send(req.body)
+   const newRecord = new Score(req.body);
+   await newRecord.save();
+   res.redirect('/');
 })
 
 
 app.listen(3000, () => {
    console.log('APP IS LISTENING ON PORT 3000')
 })
+
+//##########################        FUNCTIONS
+const findRecord = async()=> {
+   const scores = await Score.find({})
+   return (scores.reduce((prev, current)=>{
+      return (prev.score < current.score)? current : prev;
+      }))
+}
+
+const getLength = (obj) => {
+   let l = 0;
+   for(let i in obj){
+      l+=1
+   }
+   return l;
+}
